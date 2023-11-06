18f9ffec407e9aa609d5df8fd546f618acc98ff8
Keagan Larson <larske07@luther.edu>
2023-11-05 20:55:43 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict;'
+
+async function fetchData(url) {
+
+    return await fetch(url)
+    .then(response => response.json())
+    .catch(error => console.error());   
+}
+
+async function getData() {
+
+    lang = document.querySelector("#selLang").value
+    category = document.querySelector("#selCat").value
+    number = document.querySelector("#selNum").value
+
+    let url = `https://jokes-api-zpxb.onrender.com/api/v1/jokes/${lang}/${category}/${number}`;
+    console.log(url);
+
+    let jokes = await fetchData(url);
+
+    displayData(jokes.data);
+}
+
+function displayData(data) {
+    let contentDiv = document.querySelector("#jokes");
+    contentDiv.innerHTML = "";
+    for (let joke of data) {
+        let jokeElement = document.createElement("div");
+        jokeElement.innerHTML = joke;
+        jokeElement.classList.add("joke");
+        contentDiv.appendChild(jokeElement);
+    }
+}
